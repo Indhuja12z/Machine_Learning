abe56c50ba110286b91e8a8e5c66b5d9f454e43d
Indhuja12z <159168184+Indhuja12z@users.noreply.github.com>
2026-01-19 19:45:23 +0530

    adding accuracy

--- Ex-4/persona_prediction_hface.py
+++ Ex-4/persona_prediction_hface.py
@@ -1,8 +1,29 @@
 import pandas as pd
 from transformers import pipeline
 
-clf = pipeline("zero-shot-classification", model="valhalla/distilbart-mnli-12-3")
+clf = pipeline(
+    "zero-shot-classification",
+    model="valhalla/distilbart-mnli-12-3"
+)
+
 data = pd.read_csv("user_interest.csv")
 
+labels = data['Persona'].unique().tolist()
+
+correct = 0
+
+for i in range(len(data)):
+    text = data.loc[i, 'Board_Name']
+    actual = data.loc[i, 'Persona']
+
+    pred = clf(text, labels)['labels'][0]
+
+    if pred == actual:
+        correct += 1
+
+accuracy = (correct / len(data)) * 100
+
+print("Accuracy:", accuracy, "%")
+
 new_board = "Baking"
-print(clf(new_board, data['Persona'].unique().tolist())['labels'][0])
+print("Prediction:", clf(new_board, labels)['labels'][0])
