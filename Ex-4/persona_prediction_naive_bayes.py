60d225017e373af394a43084b36b3fb277f7d333
Indhuja12z <159168184+Indhuja12z@users.noreply.github.com>
2026-01-19 19:46:31 +0530

    adding accuracy

--- Ex-4/persona_prediction_naive_bayes.py
+++ Ex-4/persona_prediction_naive_bayes.py
@@ -1,26 +1,32 @@
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score
 
-# Step 1: Load dataset
 data = pd.read_csv("user_interest.csv")
 
-# Step 2: Encode categorical values
+
 le_board = LabelEncoder()
 le_persona = LabelEncoder()
 
 data['Board_Name_Encoded'] = le_board.fit_transform(data['Board_Name'])
 data['Persona_Encoded'] = le_persona.fit_transform(data['Persona'])
 
-# Step 3: Define features and target
+
 X = data[['Board_Name_Encoded']]
 y = data['Persona_Encoded']
 
-# Step 4: Train Naive Bayes model
+
 model = MultinomialNB()
 model.fit(X, y)
 
-# Step 5: Predict for a new board
+
+y_pred = model.predict(X)
+
+accuracy = accuracy_score(y, y_pred)
+print("Model Accuracy:", accuracy * 100, "%")
+
+
 new_board = "Baking"
 new_board_encoded = le_board.transform([new_board])
 
